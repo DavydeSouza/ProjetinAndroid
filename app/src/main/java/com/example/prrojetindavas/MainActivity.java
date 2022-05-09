c88c34dad0978981cd7be27e4732b1225eb43b61
Davy de Souza <davy.desouza02@gmail.com>
2022-05-09 13:20:20 -0300

    Part 5 Making the fragments work

--- app/src/main/java/com/example/prrojetindavas/MainActivity.java
+++ app/src/main/java/com/example/prrojetindavas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.prrojetindavas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.navigation.NavController;
@@ -10,19 +11,26 @@ import androidx.navigation.ui.NavigationUI;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.prrojetindavas.Activity.Login;
+import com.example.prrojetindavas.Fragments.HomeFragment;
+import com.example.prrojetindavas.Fragments.PaymentsFragment;
+import com.example.prrojetindavas.Fragments.SettingsFragment;
 import com.example.prrojetindavas.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
-    private NavHostFragment navHostFragment;
-    private NavController navController;
+    BottomNavigationView bottom_navigation;
+    HomeFragment homeFragment = new HomeFragment();
+    PaymentsFragment paymentsFragment = new PaymentsFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
 
     private FirebaseAuth mAuth;
     private Button btn_logout;
@@ -31,9 +39,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
         mAuth =FirebaseAuth.getInstance();
         btn_logout = findViewById(R.id.btn_logout);
-       // initNavigation();
+
+        bottom_navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+              switch (item.getItemId()){
+                  case R.id.menu_principal:
+                      getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                      return true;
+                  case R.id.menu_pagamento:
+                      getSupportFragmentManager().beginTransaction().replace(R.id.container,paymentsFragment).commit();
+                      return true;
+                  case R.id.menu_exit:
+                      getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
+                      return true;
+              }
+              return false;
+            }
+        });
 
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,10 +87,5 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
     }
-  /*  private void initNavigation(){
-        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
-        navController = navHostFragment.getNavController();
-        NavigationUI.setupWithNavController(binding.bottomNavigation,navController);
-    }*/
 
 }
