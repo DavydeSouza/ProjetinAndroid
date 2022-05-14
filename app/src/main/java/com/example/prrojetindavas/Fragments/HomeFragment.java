762ccf624383207cb2a92785df244acdcb842fe2
Davy de Souza <davy.desouza02@gmail.com>
2022-05-14 14:52:34 -0300

    Part 6 Adjusting main screen / profile

--- app/src/main/java/com/example/prrojetindavas/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/prrojetindavas/Fragments/HomeFragment.java
@@ -2,20 +2,49 @@ package com.example.prrojetindavas.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.prrojetindavas.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class HomeFragment extends Fragment {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private TextView nameUser;
+    View view;
+    String UserID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        nameUser = view.findViewById(R.id.nameUser);
+        return view;
     }
+    /*public void onStart(){
+        super.onStart();
+        UserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection("Usuarios").document(UserID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if(documentSnapshot != null);
+                    nameUser.setText(documentSnapshot.getString("nome"));
+            }
+        });
+    }*/
 }
--- app/src/main/java/com/example/prrojetindavas/Model/UserModel.java
+++ app/src/main/java/com/example/prrojetindavas/Model/UserModel.java
@@ -1,14 +1,27 @@
 package com.example.prrojetindavas.Model;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserModel {
     private String id;
     private String nome;
     private String sobrenome;
     private String email;
+    private String UserId;
 
     public UserModel() {
     }
@@ -53,7 +66,22 @@ public class UserModel {
         this.email = email;
     }
     public void Save(){
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        reference.child("usuarios").child(getId()).setValue(this);
+        UserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("nome",nome);
+        usuarios.put("sobrenome",sobrenome);
+        DocumentReference documentReference = db.collection("Usuarios").document(UserId);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+            Log.d("db","Sucesso ao salvar");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db","Falha ao salvar");
+            }
+        });
     }
 }
